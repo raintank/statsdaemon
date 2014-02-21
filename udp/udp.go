23470de950d260b82ef298c8c719be97ba27f7ed
Dieter Plaetinck <dieter@vimeo.com>
2014-02-21 14:34:57 -0500

    provide detailed errors on invalid lines

--- udp/udp.go
+++ udp/udp.go
@@ -2,6 +2,7 @@ package udp
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"github.com/tv42/topic"
 	"github.com/vimeo/statsdaemon/common"
@@ -14,43 +15,42 @@ const (
 	MaxUdpPacketSize = 65535
 )
 
-// ParseLine turns a line into a *Metric (or not) and returns whether the line was valid.
+// ParseLine turns a line into a *Metric (or not) and returns an error if the line was invalid.
 // note that *Metric can be nil when the line was valid (if the line was empty)
-func ParseLine(line []byte) (metric *common.Metric, valid bool) {
+func ParseLine(line []byte) (metric *common.Metric, err error) {
 	if len(line) == 0 {
-		return nil, true
+		return nil, nil
 	}
 	parts := bytes.SplitN(line, []byte(":"), 2)
 	if len(parts) != 2 {
-		return nil, false
+		return nil, errors.New("bad amount of colons")
 	}
 	if bytes.Contains(parts[1], []byte(":")) {
-		return nil, false
+		return nil, errors.New("bad amount of colons")
 	}
 	bucket := parts[0]
 	parts = bytes.SplitN(parts[1], []byte("|"), 3)
 	if len(parts) < 2 {
-		return nil, false
+		return nil, errors.New("bad amount of pipes")
 	}
 	modifier := string(parts[1])
 	if modifier != "g" && modifier != "c" && modifier != "ms" {
-		return nil, false
+		return nil, errors.New("unsupported metric type")
 	}
 	sampleRate := float64(1)
 	if len(parts) == 3 {
 		if parts[2][0] != byte('@') {
-			return nil, false
+			return nil, errors.New("invalid sampling")
 		}
 		var err error
 		sampleRate, err = strconv.ParseFloat(string(parts[2])[1:], 32)
 		if err != nil {
-			return nil, false
+			return nil, errors.New("couldn't parseFloat sampling")
 		}
 	}
 	value, err := strconv.ParseFloat(string(parts[0]), 64)
 	if err != nil {
-		log.Printf("ERROR: failed to parse value in line '%s' - %s\n", line, err)
-		return nil, false
+		return nil, errors.New("couldn't parseFloat value")
 	}
 	metric = &common.Metric{
 		Bucket:   string(bucket),
@@ -58,7 +58,7 @@ func ParseLine(line []byte) (metric *common.Metric, valid bool) {
 		Modifier: modifier,
 		Sampling: float32(sampleRate),
 	}
-	return metric, true
+	return metric, nil
 }
 
 // ParseMessage turns byte data into a slice of metric pointers
@@ -67,11 +67,11 @@ func ParseLine(line []byte) (metric *common.Metric, valid bool) {
 func ParseMessage(data []byte, prefix_internal string, invalid_lines *topic.Topic) []*common.Metric {
 	var output []*common.Metric
 	for _, line := range bytes.Split(data, []byte("\n")) {
-		metric, valid := ParseLine(line)
-		if !valid {
-            if invalid_lines != nil {
-                invalid_lines.Broadcast <- line
-            }
+		metric, err := ParseLine(line)
+		if err != nil {
+			if invalid_lines != nil {
+				invalid_lines.Broadcast <- line
+			}
 			metric = &common.Metric{
 				fmt.Sprintf("%starget_type=count.type=invalid_line.unit=Err", prefix_internal),
 				float64(1),
