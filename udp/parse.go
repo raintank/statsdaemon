8062f74337f12b6b90fa77a55424f18ae5c33145
Dieter Plaetinck <dieter@vimeo.com>
2014-10-13 11:11:23 -0400

    add new lexer-based udp line parser, also tests and benchmarks

---
+++ udp/parse.go
@@ -0,0 +1,168 @@
+package udp
+
+import (
+	"errors"
+	"github.com/vimeo/statsdaemon/common"
+	"strconv"
+)
+
+type lexer struct {
+	input []byte
+	len   int
+	start int
+	pos   int
+	m     common.Metric
+	err   error
+}
+
+// assumes we don't have \x00 bytes in input
+const eof = 0
+
+func (l *lexer) next() byte {
+	if l.pos >= l.len {
+		return eof
+	}
+	b := l.input[l.pos]
+	l.pos++
+	return b
+}
+
+func (l *lexer) run() {
+	for state := lexKeySep; state != nil; {
+		state = state(l)
+	}
+	if l.err == nil && l.m.Sampling == 0 {
+		l.m.Sampling = float32(1)
+	}
+
+}
+
+var (
+	errMissingKeySep   = errors.New("missing key separator")
+	errEmptyKey        = errors.New("key zero len")
+	errMissingValueSep = errors.New("missing value separator")
+	errInvalidModifier = errors.New("invalid modifier")
+	errInvalidSampling = errors.New("invalid sampling")
+)
+
+type stateFn func(*lexer) stateFn
+
+// lex until we find the colon separator between key and value
+func lexKeySep(l *lexer) stateFn {
+	for {
+		switch b := l.next(); b {
+		case ':':
+			return lexKey
+		case eof:
+			l.err = errMissingKeySep
+			return nil
+		}
+	}
+}
+
+// lex the key
+func lexKey(l *lexer) stateFn {
+	if l.start == l.pos-1 {
+		l.err = errEmptyKey
+		return nil
+	}
+	l.m.Bucket = string(l.input[l.start : l.pos-1])
+	l.start = l.pos
+	return lexValueSep
+}
+
+// lex until we find the pipe separator between value and modifier
+func lexValueSep(l *lexer) stateFn {
+	for {
+		// cheap check here. ParseFloat will do it.
+		switch b := l.next(); b {
+		case '|':
+			return lexValue
+		case eof:
+			l.err = errMissingValueSep
+			return nil
+		}
+	}
+}
+
+// lex the value
+func lexValue(l *lexer) stateFn {
+	v, err := strconv.ParseFloat(string(l.input[l.start:l.pos-1]), 64)
+	if err != nil {
+		l.err = err
+		return nil
+	}
+	l.m.Value = v
+	l.start = l.pos
+	return lexModifier
+}
+
+// lex the modifier
+func lexModifier(l *lexer) stateFn {
+	b := l.next()
+	switch b {
+	case 'g':
+		fallthrough
+	case 'c':
+		l.m.Modifier = string(b)
+		l.start = l.pos
+		return lexModifierSep
+	case 'm':
+		if b := l.next(); b != 's' {
+			l.err = errInvalidModifier
+			return nil
+		}
+		l.start = l.pos
+		l.m.Modifier = "ms"
+		return lexModifierSep
+	default:
+		l.err = errInvalidModifier
+		return nil
+
+	}
+}
+
+// lex the possible separator between modifier and samplerate
+func lexModifierSep(l *lexer) stateFn {
+	b := l.next()
+	switch b {
+	case eof:
+		return nil
+	case '|':
+		l.start = l.pos
+		return lexSampleRate
+	}
+	l.err = errInvalidModifier
+	return nil
+}
+
+// lex the sample rate
+func lexSampleRate(l *lexer) stateFn {
+	b := l.next()
+	if b != '@' {
+		l.err = errInvalidSampling
+	}
+	l.start = l.pos
+
+	v, err := strconv.ParseFloat(string(l.input[l.start:]), 32)
+	if err != nil {
+		l.err = err
+		return nil
+	}
+	l.m.Sampling = float32(v)
+	return nil
+}
+
+// ParseLine with lexer impl
+func ParseLine2(line []byte) (*common.Metric, error) {
+	llen := len(line)
+	if llen == 0 {
+		return nil, nil
+	}
+	l := &lexer{input: line, len: llen}
+	l.run()
+	if l.err != nil {
+		return nil, l.err
+	}
+	return &l.m, nil
+}
---
+++ udp/udp_test.go
@@ -0,0 +1,174 @@
+package udp
+
+import (
+	"errors"
+	"github.com/vimeo/statsdaemon/common"
+	"reflect"
+	"testing"
+)
+
+func runTest(t *testing.T, f func([]byte) (*common.Metric, error)) {
+	// input format: key:value|modifier[|@samplerate]
+	type Case struct {
+		Type      string
+		In        string
+		OutMetric *common.Metric
+		OutErr    []error // we allow more than one phrasing
+	}
+
+	tests := []Case{
+		Case{
+			"gauge-int-simple",
+			"search.solr.clips.results:78186|g",
+			&common.Metric{
+				Bucket:   "search.solr.clips.results",
+				Value:    78186,
+				Modifier: "g",
+				Sampling: float32(1),
+			},
+			nil,
+		},
+		Case{
+			"counter-int-simple-trailing-white",
+			"cliapp1.queue.consumer.VideoFile_PruneSourceFilesV6.processing.10_90_128_162.removed:1|c  ",
+			&common.Metric{
+				Bucket:   "cliapp1.queue.consumer.VideoFile_PruneSourceFilesV6.processing.10_90_128_162.removed",
+				Value:    1,
+				Modifier: "c",
+				Sampling: float32(1),
+			},
+			nil,
+		},
+		Case{
+			"  timing-float-with-samplerate-prefix-whitespace",
+			"lvimdfs3.object-replicator.partition.update.timing:3.69596481323|ms|@0.05",
+			&common.Metric{
+				Bucket:   "lvimdfs3.object-replicator.partition.update.timing",
+				Value:    3.69596481323,
+				Modifier: "ms",
+				Sampling: float32(0.05),
+			},
+			nil,
+		},
+		Case{
+			"funky-chars",
+			"foo%bar=yes:12|ms|@0.05",
+			&common.Metric{
+				Bucket:   "foo%bar=yes",
+				Value:    12,
+				Modifier: "ms",
+				Sampling: float32(0.05),
+			},
+			nil,
+		},
+		Case{
+			"middle-whitespace",
+			"foo bar:12|ms|@0.05",
+			&common.Metric{
+				Bucket:   "foo bar",
+				Value:    12,
+				Modifier: "ms",
+				Sampling: float32(0.05),
+			},
+			nil,
+		},
+		Case{
+			"empty-key",
+			":12|ms|@0.05",
+			nil,
+			[]error{errors.New("key zero len"), errors.New("")},
+		},
+		Case{
+			"empty-val",
+			"key:|ms|@0.05",
+			nil,
+			[]error{errors.New("strconv.ParseFloat: parsing \"\": invalid syntax")},
+		},
+		Case{
+			"empty-mod",
+			"key:12||@0.05",
+			nil,
+			[]error{errors.New("unsupported metric type"), errors.New("invalid modifier")},
+		},
+		Case{
+			"empty-samplerate",
+			"key:12|g|@",
+			nil,
+			[]error{errors.New("strconv.ParseFloat: parsing \"\": invalid syntax")},
+		},
+		Case{
+			"bad-samplerate",
+			"key:12|g|@f",
+			nil,
+			[]error{errors.New("strconv.ParseFloat: parsing \"f\": invalid syntax")},
+		},
+		Case{
+			"missing-modifier",
+			"foo_bar:12|@0.05",
+			nil,
+			[]error{errors.New("unsupported metric type"), errors.New("invalid modifier")},
+		},
+		Case{
+			"invalid-samplerate",
+			"foo_bar:12@0.05",
+			nil,
+			[]error{errors.New("bad amount of pipes"), errors.New("missing value separator")},
+		},
+	}
+
+	for _, c := range tests {
+		metric, err := f([]byte(c.In))
+		if c.OutErr == nil {
+			if err != nil {
+				t.Errorf("case %s failed\nin: %s\nexpected err: %v\nreceived err: %v\n", c.Type, c.In, c.OutErr, err)
+			}
+		} else {
+			good := false
+			for _, acceptableErr := range c.OutErr {
+				if err != nil && err.Error() == acceptableErr.Error() {
+					good = true
+					break
+				}
+			}
+			if !good {
+				t.Errorf("case %s failed\nin: %s\nexpected one of err: %v\nreceived err:         %v\n", c.Type, c.In, c.OutErr, err)
+			}
+		}
+		metricEqual := reflect.DeepEqual(c.OutMetric, metric)
+		if !metricEqual {
+			t.Errorf("case %s failed\nin: %s\nexpected metric: %v\nreceived metric: %v\n", c.Type, c.In, c.OutMetric, metric)
+		}
+	}
+}
+
+func runBench(b *testing.B, f func([]byte) (*common.Metric, error)) {
+	var err error
+	line1 := []byte("cat:12.0231|ms")
+	line2 := []byte("meow:45.0231|g|@54.12")
+	for i := 0; i < b.N; i++ {
+		_, err = f(line1)
+		if err != nil {
+			b.Fatal(err)
+		}
+		_, err = f(line2)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func TestParseLine(t *testing.T) {
+	runTest(t, ParseLine)
+}
+
+func TestParseLine2(t *testing.T) {
+	runTest(t, ParseLine2)
+}
+
+func BenchmarkParseLine(b *testing.B) {
+	runBench(b, ParseLine)
+}
+
+func BenchmarkParseLine2(b *testing.B) {
+	runBench(b, ParseLine2)
+}
